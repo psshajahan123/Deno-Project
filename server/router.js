b66e7fcca5c44dd8cd13f7b587a61a03307393a0
psshajahan123 <poolasyedshajahan123@gmail.com>
2024-02-17 22:01:31 +0530

    changed backend again

--- server/router.js
+++ server/router.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
-});
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  router.use(express.static(path.join(__dirname1, "/client/build")));
+
+  router.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+  });
+} else {
+  router.get("/", (req, res) => {
+    res.send({ response: "Server is up and running." }).status(200);
+  });
+}
 
 module.exports = router;
