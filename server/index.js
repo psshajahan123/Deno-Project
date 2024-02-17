5d3a835b78b85cea5f43053e9875e8dfd2dd857f
psshajahan123 <poolasyedshajahan123@gmail.com>
2024-02-17 21:34:50 +0530

    scripts change

--- server/index.js
+++ server/index.js
@@ -10,28 +10,28 @@ app.use(express.json())
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
-const router = express.Router();
-
+const router = require("./router");
+app.use(router);
 
 const server = http.createServer(app);
 
 //........................Deployment.................................
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send({ response: "Server is up and running." }).status(200);
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send({ response: "Server is up and running." }).status(200);
+//   });
+// }
 
 //........................Deployment.................................
-app.use(router);
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3004",
